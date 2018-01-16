f13da704ca0e55823c062fabde21cfa89e95c4f6
Alonzo <jaxonetic@gmail.com>
2018-01-16 06:03:46 -0700

    updated paths

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Resume from './resume.js';
-import registerServiceWorker from './registerServiceWorker';
-//adding the elements and data
-import {educationTestData, technologyTestData, workHistoryTestData, contactTestData} from './util.js'
+import Resume from './ResumeComponent/resume.js';
+// import registerServiceWorker from './registerServiceWorker';
 
+// adding the elements and data
+import {educationTestData, technologyTestData, workHistoryTestData, contactTestData} from './ResumeComponent/util.js'
 
+// Render the Component
 ReactDOM.render(<Resume educationHistory={educationTestData()} workHistory={workHistoryTestData()} technology={technologyTestData()}  contactInfo={contactTestData()}/>, document.getElementById('root'));
 
